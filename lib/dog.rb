a3203cc7c9d43bf35116d4ade028cf2a93068cc1
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-24 21:09:40 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog 
+ def bark
+   puts "woof!"
+  end
+  
+
+end
+class Dog
+  def name=(dogs_name)
+    @name = dogs_name
+  end 
+
+  def name
+    @name
+  end 
+  
+end 
